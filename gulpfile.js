e7aa8cc68796b3f3ab0d83b70f5bffbc862c7b80
Daniel Wildman <dan@Daniels-MacBook-Pro.local>
2018-02-16 12:11:28 +0000

    added gulp build tasks

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    bs   = require('browser-sync').create();
+
+gulp.task('sync', function() {
+    bs.init({
+        server: {
+            baseDir: './'
+        },
+        open: false,
+        ghostMode: false
+    });
+});
+
+gulp.task('watch', ['sync'], function() {
+    gulp.watch('./css/*.css', ['css']);
+    gulp.watch('./*.html').on('change', bs.reload);
+});
+
+gulp.task('css', function() {
+    return gulp.src('./css/*.css')
+        .pipe(bs.reload({stream: true}))
+})
